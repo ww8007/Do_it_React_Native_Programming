2266944930c1546793ce3741e8eb10907d81dd2a
ww8007 <wshmin@naver.com>
2021-05-31 01:47:56 +0900

    Make Dummy Data

--- ch02/ch02_1/App.tsx
+++ ch02/ch02_1/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
-
+import * as D from './src/data';
 export default function App() {
   const isLoading = true;
-  const children = isLoading ? (
-    <Text>Loading...</Text>
-  ) : (
-    <Text>Hello JSX!</Text>
-  );
-  return <SafeAreaView>{children}</SafeAreaView>;
+  const person = D.createRandomPerson();
+  if (isLoading) {
+    return (
+      <SafeAreaView>
+        <Text>{JSON.stringify(person, null, 2)}</Text>
+      </SafeAreaView>
+    );
+  }
 }
