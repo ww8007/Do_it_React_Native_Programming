a8ebd56da9757427dfe1c6efc5a216e59c40ca48
ww8007 <wshmin@naver.com>
2021-05-30 23:23:46 +0900

    변수에 jsx 담기

--- ch02/ch02_1/App.tsx
+++ ch02/ch02_1/App.tsx
@@ -3,11 +3,10 @@ import {Text, SafeAreaView} from 'react-native';
 
 export default function App() {
   const isLoading = true;
-
-  return (
-    <SafeAreaView>
-      {isLoading && <Text>Hellow Jsx</Text>}
-      {!isLoading && <Text>Not Render</Text>}
-    </SafeAreaView>
+  const children = isLoading ? (
+    <Text>Loading...</Text>
+  ) : (
+    <Text>Hello JSX!</Text>
   );
+  return <SafeAreaView>{children}</SafeAreaView>;
 }
