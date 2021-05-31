522ae08c50f3f0d43fb48d9b16d2d6f7c14cffd5
ww8007 <wshmin@naver.com>
2021-05-31 17:38:10 +0900

    Make FC type tsx

--- ch02/ch02_1/App.tsx
+++ ch02/ch02_1/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {ArrowComponet} from './src/screens/ArrowComponet';
+import Person from './src/screens/Person';
+import * as D from './src/data';
 export default function App() {
+  const person = D.createRandomPerson();
+  React.createContext;
   return (
     <SafeAreaView>
       <ArrowComponet />
+      <Person person={person} />
     </SafeAreaView>
   );
 }
